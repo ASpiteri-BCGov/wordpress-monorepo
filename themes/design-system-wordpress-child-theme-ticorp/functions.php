1ec3edba0fe71de01b011b9e0b2c833778399400
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-05-20 16:25:20 -0700

    fix headers for consistency

--- themes/design-system-wordpress-child-theme-ticorp/functions.php
+++ themes/design-system-wordpress-child-theme-ticorp/functions.php
@@ -5,8 +5,6 @@
  * @package Design_System_WordPress_Child_Theme_TI_Corp
  */
 
-// This file is required for WordPress theme detection and PHPUnit testing.
-
 /**
  * Load Composer autoloader and verify required class exists.
  * If the autoloader or the required class is missing, halt plugin execution.
