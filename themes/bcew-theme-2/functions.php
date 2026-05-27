01cd3e277ed4f8fb4fd4fb7361395ac6a7e39bff
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-05-27 15:27:42 -0700

    Update functions.php

--- themes/bcew-theme-2/functions.php
+++ themes/bcew-theme-2/functions.php
@@ -6,11 +6,21 @@
  */
 
 /**
+ * Disables the default patterns from WordPress.
+ */
+function design_system_disable_default_block_patterns()
+{
+    remove_theme_support('core-block-patterns');
+}
+add_action('init', 'design_system_disable_default_block_patterns');
+
+
+/**
  * Load Composer autoloader and verify required class exists.
  * If the autoloader or the required class is missing, halt plugin execution.
  */
 $autoloader_path = __DIR__ . '/vendor/autoload.php';
-if ( file_exists( $autoloader_path ) ) {
+if (file_exists($autoloader_path)) {
     require_once $autoloader_path;
 }
 
@@ -21,18 +31,19 @@ if ( file_exists( $autoloader_path ) ) {
  *
  * @return WP_Theme_JSON_Data
  */
-function extended_web_enable_appearance_tools_for_super_admins( $theme_json ) {
-    if ( ! is_super_admin() ) {
+function extended_web_enable_appearance_tools_for_super_admins($theme_json)
+{
+    if (!is_super_admin()) {
         return $theme_json;
     }
 
     return $theme_json->update_with(
         array(
-            'version'  => 3,
+            'version' => 3,
             'settings' => array(
                 'appearanceTools' => true,
             ),
         )
     );
 }
-add_filter( 'wp_theme_json_data_theme', 'extended_web_enable_appearance_tools_for_super_admins', 20 );
+add_filter('wp_theme_json_data_theme', 'extended_web_enable_appearance_tools_for_super_admins', 20);
